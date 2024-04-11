8f9e3bce63b096a2323bba4daab5e2e6745e2998
Tikam Rai <a00280792@mycambrian.ca>
2024-04-11 03:57:07 +0000

    Near final commit. Some task still reamain.

--- WMS.ConsoleApp/Program.cs
+++ WMS.ConsoleApp/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using WMS.Library;
+using WMS.Library.Factory;
+using WMS.Library.Observers;
+
+namespace WMS.ConsoleApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        WeatherData weatherData = WeatherData.GetInstance();
+
+        weatherData.RandomUpdate(5000);
+
+        WeatherStation weatherStation = new WeatherStation(weatherData);
+
+        var currentConditionsDisplay = weatherStation.CreateDisplay("CurrentConditions");
+        var statisticsDisplay = weatherStation.CreateDisplay("Statistics");
+        var forecastDisplay = weatherStation.CreateDisplay("Forecast");
+
+        Task.Delay(2000).Wait();
+        Console.WriteLine("Weather updates are ongping. Press any key to stop and exit...");
+        Console.ReadKey();
+    }
+}
